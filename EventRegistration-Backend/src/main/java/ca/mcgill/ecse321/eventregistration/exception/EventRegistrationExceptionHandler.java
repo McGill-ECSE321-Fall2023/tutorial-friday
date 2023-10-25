6f3a78cb1f2016e884dabb33ac31c8ed48d93134
Louis Hildebrand <louis.hildebrand@mcgill.ca>
2023-10-25 19:01:47 -0400

    Add bean validation to Person class

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/exception/EventRegistrationExceptionHandler.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/exception/EventRegistrationExceptionHandler.java
@@ -1,12 +1,16 @@
 package ca.mcgill.ecse321.eventregistration.exception;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import ca.mcgill.ecse321.eventregistration.dto.ErrorDto;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class EventRegistrationExceptionHandler {
@@ -14,5 +18,15 @@ public class EventRegistrationExceptionHandler {
 	public ResponseEntity<ErrorDto> handleEventRegistrationException(EventRegistrationException exception) {
 		ErrorDto body = new ErrorDto(Arrays.asList(exception.getMessage()));
 		return new ResponseEntity<ErrorDto>(body, exception.getStatus());
+	}
+	
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<ErrorDto> handleConstraintViolationException(ConstraintViolationException exception) {
+		ArrayList<String> errors = new ArrayList<String>();
+		for (ConstraintViolation<?> cv : exception.getConstraintViolations()) {
+			errors.add(cv.getMessage());
+		}
+		ErrorDto body = new ErrorDto(errors);
+		return new ResponseEntity<ErrorDto>(body, HttpStatus.BAD_REQUEST);
 	}
 }
--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Person.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Person.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Person {
@@ -16,7 +19,10 @@ public class Person {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	@NotBlank(message = "Every person must have a non-empty name.")
 	private String name;
+	@Size(min = 8, message = "Passwords must be at least eight characters long.")
+	@NotNull(message = "Passwords must not be null.")
 	private String password;
 	private boolean isVerified;
 
