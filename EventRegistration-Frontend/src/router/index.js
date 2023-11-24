6558fb099edd12464785bd83369dbe4c4923788a
Louis Hildebrand <louis.hildebrand@mcgill.ca>
2023-11-24 18:27:23 -0500

    Add working navigation guard example

--- EventRegistration-Frontend/src/router/index.js
+++ EventRegistration-Frontend/src/router/index.js
@@ -10,7 +10,13 @@ export default new Router({
     {
       path: '/hello',
       name: 'Hello',
-      component: Hello
+      component: Hello,
+      beforeEnter: (from, to, next) => {
+        alert("Are you sure you're allowed to access this page?");
+        next();
+        // Or call next(false) to abort the navigation. See the docs at
+        // https://v3.router.vuejs.org/guide/advanced/navigation-guards.html
+      }
     },
     {
       path: '/',
