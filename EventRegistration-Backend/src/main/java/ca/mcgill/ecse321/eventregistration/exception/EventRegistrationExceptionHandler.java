c8bba77513a4967c52755a862c4811a6557ba989
Louis Hildebrand <louis.hildebrand@mcgill.ca>
2023-11-24 18:17:09 -0500

    Fix problem with backend exception handler

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/exception/EventRegistrationExceptionHandler.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/exception/EventRegistrationExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import ca.mcgill.ecse321.eventregistration.dto.ErrorDto;
 import jakarta.validation.ConstraintViolation;
@@ -23,13 +22,13 @@ public class EventRegistrationExceptionHandler {
 	}
 	
 	@ExceptionHandler(ConstraintViolationException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ErrorDto handleConstraintViolationException(ConstraintViolationException ex) {
+	// WARNING: Using @RequestStatus here can cause issues
+	public ResponseEntity<ErrorDto> handleConstraintViolationException(ConstraintViolationException ex) {
 		ArrayList<String> errorMessages = new ArrayList<String>();
 		for (ConstraintViolation<?> cv : ex.getConstraintViolations()) {
 			errorMessages.add(cv.getMessage());
 		}
 		ErrorDto responseBody = new ErrorDto(errorMessages);
-		return responseBody;
+		return new ResponseEntity<ErrorDto>(responseBody, HttpStatus.BAD_REQUEST);
 	}
 }
